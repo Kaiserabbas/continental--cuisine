27e0f3c080b7ea30593d0a6000da388fd3bff85a
Qaisar <kayser.abbas@gmail.com>
2023-09-01 13:57:10 +0500

    Modify the default App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Meals from './components/Meals';
+import CuisineDropdown from './components/CuisineDropdown';
+
+import './styles.css';
+
 
 function App() {
+  const handleCuisineChange = (selectedCuisine) => {
+    const FOOD_BASE_URL = `https://themealdb.com/api/json/v1/1/filter.php?a=${selectedCuisine}`;
+    console.log(FOOD_BASE_URL);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className='app'>
+              <CuisineDropdown onCuisineChange={handleCuisineChange} />
+
+        <h1>Fusion  Bites Resturant</h1>
+
+        <Meals />
+      </div>
+    </Provider>
   );
 }
 
