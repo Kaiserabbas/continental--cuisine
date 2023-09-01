99d458a821602c9de4e58adffd5dfd51c6956997
Qaisar <kayser.abbas@gmail.com>
2023-09-01 15:03:12 +0500

    Update the code for design requirement

--- src/components/CuisineDropdown.js
+++ src/components/CuisineDropdown.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchMeals } from '../actions/mealActions';
+import logo from '../images/logo.png';
 
 function CuisineDropdown() {
   const [cuisine, setCuisine] = useState('Indian');
@@ -13,14 +14,17 @@ function CuisineDropdown() {
   };
 
   return (
-    <div>
-      <label>Select Cuisine: </label>
-      <select value={cuisine} onChange={handleCuisineChange}>
-        <option value='Indian'>Indian</option>
-        <option value='American'>American</option>
-        <option value='Mexican'>Mexican</option>
-        <option value='Canadian'>Canadian</option>
+    <div className='navbar'>
+     <img src={logo} alt='logo' className='logo' /> 
+     <div className='cuisine'>
+      <label className='label'>Select Cuisine: </label>
+      <select value={cuisine} onChange={handleCuisineChange} className='select'>
+        <option value='Indian' className='drop-list'>Pakistani</option>
+        <option value='American' className='drop-list'>American</option>
+        <option value='Mexican' className='drop-list'>Mexican</option>
+        <option value='Canadian' className='drop-list'>Canadian</option>
       </select>
+     </div>
     </div>
   );
 }
